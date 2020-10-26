1c73005d2eb763652d76cb138ca4ef5b42fc6236
Marcus Benjamin Ravnå Birkeland <36818485+Zprite@users.noreply.github.com>
2020-10-26 13:29:39 +0100

    Fixed sorting bug in chrome

--- assets/scripts/menuSort.js
+++ assets/scripts/menuSort.js
@@ -5,7 +5,16 @@ function getRenderMode(){
 
 function sortAlphapetically(menu){
   let menuClone = Array.from(menu);
-  menuClone.sort((a,b)=> a.name > b.name );
+  menuClone.sort((a,b)=> {
+    if(a.name > b.name){
+      return 1;
+    }
+    else if(a.name < b.name){
+      return -1;
+    }
+    return 0;
+  }
+  );
   console.log(menuClone);
   return menuClone;
 }
